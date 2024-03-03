2d068989379de27e775a12f1ea399832b38e2c90
AliElamir <ali.elamiir.a@gmail.com>
2024-03-03 11:16:59 +0300

    fix addmenu button

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -5,18 +5,20 @@ import { useState, useEffect } from 'react'
 const Restaurant = ({ user }) => {
   let navigate = useNavigate()
 
+  const [menus, setMenus] = useState(null)
 
   const createNewMenu = async () => {
     await Client.post('/rest/newMenu', user)
     navigate(`/menu/${user.restId._id}`)
   }
 
-  // useEffect(() => {
-  //   if (menuExist === false) {
-  //     createNewMenu()
-  //   }
-  // }, [])
-
+  useEffect(() => {
+    const getNewMenu = async () => {
+      const response = await Client.get(`/rest/newMenu/${user._id}`)
+      setMenus(response.data)
+    }
+    getNewMenu()
+  }, [])
 
   return (
     <>
@@ -28,7 +30,7 @@ const Restaurant = ({ user }) => {
         <h6>Contact: {user.contact}</h6>
       </div>
       <div>
-        {user.restId.menu === null ? (
+        {menus === false ? (
           <button onClick={createNewMenu}>Create Menu</button>
         ) : (
           <h2>Menu Already Exists</h2>
